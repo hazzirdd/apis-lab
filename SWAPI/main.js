fdadd1813f04ec6a1d31d9f16bc308017bd83334
Hayden Sweet <haydensweet22@gmail.com>
2022-05-04 15:00:45 -0600

    added comment

--- SWAPI/main.js
+++ SWAPI/main.js
@@ -1,15 +1,15 @@
 const button = document.querySelector('button');
 const body = document.getElementById("body");
 
+
+//it Works when you click the button, but the names are very slow to show up. I'm not sure why.
 buttonClicked = () => {
     clearResidents()
 
     axios.get('https://swapi.dev/api/planets/2')
     .then(res => {
-        console.log(res.data.residents)
         let residents = res.data.residents
         for (let i = 0; i < residents.length; i++) {
-            console.log(residents[i])
             axios.get(residents[i])
             .then(res => {
                 let resident = document.createElement('h2');
@@ -25,8 +25,8 @@ buttonClicked = () => {
 }
 
 clearResidents = () => {
-    const body = document.querySelector('#body')
-    body.innerHTML = ``
+    const body = document.querySelector('#body');
+    body.innerHTML = ``;
 }
 
 
